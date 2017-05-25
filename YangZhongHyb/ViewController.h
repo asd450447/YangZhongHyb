ca2bbce9c388153daf27c4cc1260fbbd73b50026
asd450447 <709089918@qq.com>
2017-05-25 14:48:01 +0800

    Initial Commit

---
+++ YangZhongHyb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YangZhongHyb
+//
+//  Created by mao ke on 2017/5/25.
+//  Copyright © 2017年 mao ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YangZhongHyb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YangZhongHyb
+//
+//  Created by mao ke on 2017/5/25.
+//  Copyright © 2017年 mao ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
